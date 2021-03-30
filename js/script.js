0d8ee67e4bd4ad07d819dbc1ec9e14c795812fbc
Jakub Haliniak <jakubh.fake@gmail.com>
2021-03-30 13:08:17 +0200

    Add object 'this'

--- js/script.js
+++ js/script.js
@@ -2,15 +2,17 @@
 
 const titleClickHandler = function (event) {
   console.log("Link was clicked!");
+  const clickedElement = this;
   console.log(event);
-
-  /* remove class 'active' from all article links */
+  /* [DONE] remove class 'active' from all article links */
   const activeLinks = document.querySelectorAll(".titles a.active");
   for (let activeLink of activeLinks) {
     activeLink.classList.remove("active");
   }
-  /* add class 'active' to clicked link */
-  /* remove class 'active' from all articles */
+  /* [IN PROGRESS] add class 'active' to clicked link */
+  console.log("clickedElement:", clickedElement);
+
+  /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll(".posts article.active");
   for (let activeArticle of activeArticles) {
     activeArticle.classList.remove("active");
