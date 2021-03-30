eb92dbf746ba392e61a3ee8ca295639cabb57dd6
Jakub Haliniak <jakubh.fake@gmail.com>
2021-03-30 18:10:14 +0200

    Add finder of articles id

--- js/script.js
+++ js/script.js
@@ -4,14 +4,14 @@ const titleClickHandler = function (event) {
   event.preventDefault();
   const clickedElement = this;
   console.log("Link was clicked!");
-  console.log(event);
+  /*console.log(event);*/
   /* [DONE] remove class 'active' from all article links */
   const activeLinks = document.querySelectorAll(".titles a.active");
   for (let activeLink of activeLinks) {
     activeLink.classList.remove("active");
   }
   /* [DONE] add class 'active' to clicked link */
-  console.log("clickedElement:", clickedElement);
+  /*console.log("clickedElement:", clickedElement);*/
   clickedElement.classList.add("active");
 
   /* [DONE] remove class 'active' from all articles */
@@ -19,13 +19,16 @@ const titleClickHandler = function (event) {
   for (let activeArticle of activeArticles) {
     activeArticle.classList.remove("active");
   }
-  /* [IN PROGRESS] get 'href' attribute from the clicked link */
+  /* [DONE] get 'href' attribute from the clicked link */
 
   const hrefLink = clickedElement.getAttribute("href");
   console.log("href linku to; ", hrefLink);
 
-  /* find the correct article using the selector (value of 'href' attribute) */
-  /* add class 'active to the correct article */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+  const idArticle = document.querySelector(hrefLink);
+  console.log(idArticle);
+
+  /* [IN PROGRESS] add class 'active to the correct article */
 };
 
 const links = document.querySelectorAll(".titles a");
