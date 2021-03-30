e3f6af6a3a04693bb1e3148569b1b12357b15f13
Jakub Haliniak <jakubh.fake@gmail.com>
2021-03-30 22:22:44 +0200

    Task 6.3 include corrections from mentor 1

--- js/script.js
+++ js/script.js
@@ -1,33 +1,34 @@
 "use strict";
+{
+  const titleClickHandler = function (event) {
+    event.preventDefault();
+    const clickedElement = this;
+    console.log("Link was clicked!");
 
-const titleClickHandler = function (event) {
-  event.preventDefault();
-  const clickedElement = this;
-  console.log("Link was clicked!");
+    const activeLinks = document.querySelectorAll(".titles a.active");
+    for (let activeLink of activeLinks) {
+      activeLink.classList.remove("active");
+    }
 
-  const activeLinks = document.querySelectorAll(".titles a.active");
-  for (let activeLink of activeLinks) {
-    activeLink.classList.remove("active");
+    clickedElement.classList.add("active");
+
+    const activeArticles = document.querySelectorAll(".posts article.active");
+    for (let activeArticle of activeArticles) {
+      activeArticle.classList.remove("active");
+    }
+
+    const hrefLink = clickedElement.getAttribute("href");
+    console.log("href linku to; ", hrefLink);
+
+    const idArticle = document.querySelector(hrefLink);
+    console.log(idArticle);
+
+    idArticle.classList.add("active");
+  };
+
+  const links = document.querySelectorAll(".titles a");
+
+  for (let link of links) {
+    link.addEventListener("click", titleClickHandler);
   }
-
-  clickedElement.classList.add("active");
-
-  const activeArticles = document.querySelectorAll(".posts article.active");
-  for (let activeArticle of activeArticles) {
-    activeArticle.classList.remove("active");
-  }
-
-  const hrefLink = clickedElement.getAttribute("href");
-  console.log("href linku to; ", hrefLink);
-
-  const idArticle = document.querySelector(hrefLink);
-  console.log(idArticle);
-
-  idArticle.classList.add("active");
-};
-
-const links = document.querySelectorAll(".titles a");
-
-for (let link of links) {
-  link.addEventListener("click", titleClickHandler);
 }
