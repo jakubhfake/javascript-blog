497180e354229d877706506d003e688a4755f250
Jakub Haliniak <jakubh.fake@gmail.com>
2021-03-30 14:56:39 +0200

    Add get 'href' attribute from the clicked link

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ const titleClickHandler = function (event) {
   for (let activeLink of activeLinks) {
     activeLink.classList.remove("active");
   }
-  /* [IN PROGRESS] add class 'active' to clicked link */
+  /* [DONE] add class 'active' to clicked link */
   console.log("clickedElement:", clickedElement);
   clickedElement.classList.add("active");
 
@@ -19,7 +19,11 @@ const titleClickHandler = function (event) {
   for (let activeArticle of activeArticles) {
     activeArticle.classList.remove("active");
   }
-  /* get 'href' attribute from the clicked articles */
+  /* [IN PROGRESS] get 'href' attribute from the clicked link */
+
+  const hrefLink = clickedElement.getAttribute("href");
+  console.log("href linku to; ", hrefLink);
+
   /* find the correct article using the selector (value of 'href' attribute) */
   /* add class 'active to the correct article */
 };
