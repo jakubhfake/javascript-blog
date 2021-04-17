e96110b9901e9f3b49e050b124854f0bfb9eb01f
Jakub Haliniak <jakubh.fake@gmail.com>
2021-04-17 14:47:58 +0200

    Delete algorithms descriptions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 /* eslint-disable indent */
 'use strict';
+// Handlebars templates
+const templates = {
+  articleLink: Handlebars.compile(document.querySelector('#template-article-link').innerHTML),
+};
 
 // Const
 
@@ -14,7 +18,6 @@ const opts = {
   cloudClassCount: 5,
   cloudClassPrefix: 'tag-size-',
 };
-// Dokończyć dodawanie obiektów zgodnie z wytycznymi na końcu modułu 7.3 
 
 const titleClickHandler = function (event) {
   event.preventDefault();
@@ -52,8 +55,11 @@ function generateTitleLinks(customSelector = '') {
   for (let article of articles) {
     const articleId = article.getAttribute('id');
     const articleTitle = article.querySelector(opts.titleSelector).innerHTML;
-    const linkHTML = '<li><a href="#' + articleId +'"><span>' + articleTitle + '</span></a></li>';
+    //const linkHTML = '<li><a href="#' + articleId +'"><span>' + articleTitle + '</span></a></li>';
+    const linkHTMLData = {id: articleId, title: articleTitle};
+    const linkHTML = templates.articleLink(linkHTMLData);
     html = html + linkHTML;
+    //console.log('handle html', html);
   }
   titleList.innerHTML = html;
   const links = document.querySelectorAll('.titles a');
@@ -122,89 +128,49 @@ function calculateTagsParams(tags){
   }
   return params;
 }
-//function calculateTagClass, zastanowić się jak zapisać ilość unikalnych tagów 
  
   function calculateTagClass(count, params){
     const normalizedCount = count - params.min;
-   //console.log('count: ', count, 'params.min: ', params.min);
-    //console.log(normalizedCount);
     const averageTagCount = params.max - params.min;
     const percentageClassValue = normalizedCount / averageTagCount;
     const classNumber = Math.round(percentageClassValue * (opts.cloudClassCount -1) + 1);
-    //console.log('class number; ', classNumber);
     return opts.cloudClassPrefix + classNumber;
     
   }
   
 
 function generateListTags() {
-    /* [NEW] create a new variable allTags with an empty object */
- let allTags = {};
-
-  /* find all articles */
+  let allTags = {};
   const articles = document.querySelectorAll(opts.articleSelector);
   
-  /* START LOOP: for every article: */
   for(let article of articles) {
-    /* find tags wrapper */
     const tagsWrapper = document.querySelector(opts.tagsSelector);
-    /* make html variable with empty string */
     tagsWrapper.innerHTML ='';
-    let html = '';    
-    /* get tags from data-tags attribute */
+    let html = '';
     const articleTags = article.getAttribute(opts.dataTags);
-    /* split tags into array */
     const articleTagsArray = articleTags.split(' ');
-    /* START LOOP: for each tag */
     for(let tag of articleTagsArray) {
-
-      /* generate HTML of the link */
       const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>';
-
-      /* add generated code to html variable */
       html = linkHTML;
-
-      /* [NEW] check if this link is NOT already in allTags */
       if(!allTags[tag]) {
-        /* [NEW] add generated code to allTags array */
         allTags[tag] = 1;
       } 
       else {
         allTags[tag]++;
       }
- 
-      /* END LOOP: for each tag */
     }
-    /* insert HTML of all the links into the tags wrapper */
     tagsWrapper.innerHTML = html;
     console.log('linki html: ', tagsWrapper.innerHTML);
-
-    /* END LOOP: for every article: */
   }
-    /* [NEW] find list of tags in right column */
     const tagsWrapper = document.querySelector('.tags');
-
-
     const tagsParams = calculateTagsParams(allTags);
-    
-    /* [NEW] create variable for all links HTML code */
     let allTagsHTML = '';
-
-    /* [NEW] START LOOP: for each tag in allTags: */
     for(let tag in allTags){
-   /* [NEW] generate code of a link and add it to allTagsHTML */
     allTagsHTML += '<li><a href="#tag-' + tag + '" class="' + calculateTagClass(allTags[tag],tagsParams) + '">' + tag /*+ '(' + allTags[tag] + ')' */+ '</a></li>';
    }
-   /* [NEW] END LOOP: for each tag in allTags: */
    tagsWrapper.innerHTML = allTagsHTML;
-   //console.log('allTags[tag] xxx:', allTags, 'tagsParams xxx: ', tagsParams);
-   //console.log(tagsWrapper);
 }
 generateListTags();
-
-// dla czego musiałem przenieść całą chmurę przed addClickListener, 
-// czy JS nie powinien odczytać tej funkcji skoro wygenerował tagi przed kliknęciem??
-// może to wina jakiegoś zakresu inicjacji funkcji lub querySelectora, 
 
 function addClickListenersToTags() {
   const linkTags = document.querySelectorAll('a[href^="#tag-"]');
@@ -220,23 +186,15 @@ addClickListenersToTags();
 // Article Authors
   
   function generateAuthors() {
-    /* [DONE] find all articles with author*/
     const articles = document.querySelectorAll(opts.articleSelector);
-    /* [DONE] START LOOP: for every article: */
     for(let article of articles) {
-      /* [DONE] find author in wrapper */
       const authorWrapper = article.querySelector(opts.articleAuthorSelector);
-      /* [DONE]  make html variable with empty string */
       authorWrapper.innerHTML = '';
       let html = '';
-      /* [DONE] get tags from data-author attribute */
       const authorTag = article.getAttribute('data-author');
-      /* [DONE] insert HTML author name to all article into the tags wrapper */
       const linkAuthorHTML = '<a href="#author-' + authorTag + '">by ' + authorTag + '</a>'; 
       html = linkAuthorHTML;
-      authorWrapper.innerHTML = html;
-    /* [DONE] END LOOP: for every article: */
-    
+      authorWrapper.innerHTML = html;    
   }
 }
   generateAuthors();
@@ -263,18 +221,9 @@ generateTitleLinks('[data-author="' + author + '"]');
 }
 
 function addClickListenerToAuthors () {
-/* find all links to tags */
 const linkAuthors = document.querySelectorAll('.post-author a[href^="#author-"]');
-  /* START LOOP: for each link */
   for(let linkAuthor of linkAuthors) {
-    /* add tagClickHandler as event listener for that link */
     linkAuthor.addEventListener('click', authorClickHandler);
-  /* END LOOP: for each link */
   }
 }
 addClickListenerToAuthors();
-
-
-
-/* POmyśleć jak dodać style scss zależnie od ilości klas w JS
-/* Don't forget delete html code !!!!!!!*/
